002bc4ca65ddfefbfac46648773aed456705d19e
yunsful <bailey05280@naver.com>
2025-01-10 20:10:50 +0900

    feat: 카테고리 엔티티 추가

---
+++ src/main/java/TImeCAlling/spring/domain/Category.java
@@ -0,0 +1,22 @@
+package TImeCAlling.spring.domain;
+
+import TImeCAlling.spring.domain.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Category extends BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, length = 10)
+    private String type;
+
+
+}
