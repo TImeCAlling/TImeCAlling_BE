42cb1158c49591a6e9574d98b85de97ecb3382f9
KYS <ehcl1027@gmail.com>
2025-01-13 02:22:59 +0900

    feat: 푸시 메세지 세팅 CRUD 구현

---
+++ src/main/java/TImeCAlling/spring/repository/pushMessageSetting/PushMessageSettingRepository.java
@@ -0,0 +1,11 @@
+package TImeCAlling.spring.repository.pushMessageSetting;
+
+import TImeCAlling.spring.domain.PushMessageSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PushMessageSettingRepository extends JpaRepository<PushMessageSetting, Long> {
+
+    List<PushMessageSetting> findByUserId(Long userId);
+}
