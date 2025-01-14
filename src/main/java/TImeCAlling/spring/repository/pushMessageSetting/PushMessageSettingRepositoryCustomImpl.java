0860d3bc72c45c4d46603a2b2ca478f01007a5cb
KYS <ehcl1027@gmail.com>
2025-01-14 17:37:50 +0900

    test: 푸시 메세지 커스텀 리포지토리 단위 테스트 구현

--- src/main/java/TImeCAlling/spring/repository/pushMessageSetting/PushMessageSettingRepositoryCustomImpl.java
+++ src/main/java/TImeCAlling/spring/repository/pushMessageSetting/PushMessageSettingRepositoryCustomImpl.java
@@ -15,14 +15,26 @@ public class PushMessageSettingRepositoryCustomImpl implements PushMessageSettin
 
     @Override
     public List<PushMessageSetting> findByUserIdOrThrow(Long userId) {
+
+//        유저의 존재 여부 확인
+        Long userCount = entityManager
+                .createQuery("SELECT COUNT(u) FROM User u WHERE u.id = :userId", Long.class)
+                .setParameter("userId", userId)
+                .getSingleResult();
+
+        if (userCount == 0) {
+            throw new UserHandler(ErrorStatus.USER_NOT_FOUND);
+        }
+
         List<PushMessageSetting> settings = entityManager
                 .createQuery("SELECT p FROM PushMessageSetting p JOIN p.user u WHERE u.id = :userId", PushMessageSetting.class)
                 .setParameter("userId", userId)
                 .getResultList();
 
-        if (settings.isEmpty()) {
-            throw new UserHandler(ErrorStatus.USER_NOT_FOUND);
-        }
+//        유저가 있음에도 푸시 메세지 세팅이 없는 경우 -> 정상 (있을 수 있다! 그냥 빈 리스트 출력)
+//        if (settings.isEmpty()) {
+//            throw new PushMessageSettingHandler(ErrorStatus.PUSH_SETTING_NOT_FOUND);
+//        }
 
         return settings;
     }
