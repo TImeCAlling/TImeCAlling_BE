e403fe7dd27717fea4f84ee09dc4c44f620533c7
yunsful <bailey05280@naver.com>
2025-01-17 18:17:25 +0900

    feat: 일정 로직 관련 레포 추가

---
+++ src/main/java/TImeCAlling/spring/repository/schedule/ChecklistRepository.java
@@ -0,0 +1,8 @@
+package TImeCAlling.spring.repository.schedule;
+
+import TImeCAlling.spring.domain.Checklist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
+
+}
