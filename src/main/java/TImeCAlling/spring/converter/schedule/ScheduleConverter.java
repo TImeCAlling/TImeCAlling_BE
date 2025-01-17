a37206111b7c349e3e6d3980ad7115a52618a53c
yunsful <bailey05280@naver.com>
2025-01-17 18:15:43 +0900

    chore: ScheduleConverter 수정

--- src/main/java/TImeCAlling/spring/converter/schedule/ScheduleConverter.java
+++ src/main/java/TImeCAlling/spring/converter/schedule/ScheduleConverter.java
@@ -3,6 +3,7 @@ package TImeCAlling.spring.converter.schedule;
 import TImeCAlling.spring.domain.Category;
 import TImeCAlling.spring.domain.Schedule;
 import TImeCAlling.spring.domain.User;
+import TImeCAlling.spring.domain.enums.FreeTime;
 import TImeCAlling.spring.domain.enums.Spare;
 import TImeCAlling.spring.web.dto.schedule.ScheduleRequestDTO;
 import TImeCAlling.spring.web.dto.schedule.ScheduleResponseDTO;
@@ -21,29 +22,19 @@ public class ScheduleConverter {
     }
 
     public static Schedule toSchedule(User user, ScheduleRequestDTO.ScheduleCreateDTO request){
-        Spare spare = Spare.valueOf(request.getSpare());
-//        switch (request.getSpare()){
-//            case "딱딱":
-//                spare = Spare.ON_TIME;
-//                break;
-//            case "여유":
-//                spare = Spare.FIVE_TO_TEN_MIN;
-//                break;
-//            case "넉넉":
-//                spare = Spare.MORE_THAN_10_MIN;
-//                break;
-//        }
+        
+        FreeTime freeTime = FreeTime.valueOf(request.getFreeTime());
 
         return Schedule.builder()
                 .user(user)
                 .name(request.getName())
                 .body(request.getBody())
-                .meetTime(request.getMeetTime())
+                .meetTime(request.getMeetTime().toLocalTime())
                 .place(request.getPlace())
                 .longitude(request.getLongitude())
                 .latitude(request.getLatitude())
                 .moveTime(request.getMoveTime())
-                .spare(spare)
+                .freeTime(freeTime)
                 .isRepeat(request.getIsRepeat())
                 .build();
     }
@@ -62,7 +53,7 @@ public class ScheduleConverter {
                 .isRepeat(schedule.getIsRepeat())
                 .place(schedule.getPlace())
                 .body(schedule.getBody())
-                .spare(schedule.getSpare())
+                .freeTime(schedule.getFreeTime())
                 .categories(categoryDTOS)
                 .build();
     }
@@ -74,7 +65,7 @@ public class ScheduleConverter {
                 .isRepeat(schedule.getIsRepeat())
                 .place(schedule.getPlace())
                 .body(schedule.getBody())
-                .spare(schedule.getSpare())
+                .freeTime(schedule.getFreeTime())
                 .build();
     }
 
