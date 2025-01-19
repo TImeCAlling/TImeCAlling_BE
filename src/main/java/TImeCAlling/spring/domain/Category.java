303c4f9995f2a696067bb2590f295ec121d692d1
m020202 <m020202@naver.com>
2025-01-19 14:47:09 +0900

    Chore: Category 엔티티 값 타입 클래스로 재정의

--- src/main/java/TImeCAlling/spring/domain/Category.java
+++ src/main/java/TImeCAlling/spring/domain/Category.java
@@ -4,24 +4,15 @@ import TImeCAlling.spring.domain.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
+@Embeddable
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class Category extends BaseEntity {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
     @Column(nullable = false, length = 10)
     private String name;
 
     @Column(nullable = false, length = 10)
     private String color;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "schedule_id")
-    private Schedule schedule;
 }
