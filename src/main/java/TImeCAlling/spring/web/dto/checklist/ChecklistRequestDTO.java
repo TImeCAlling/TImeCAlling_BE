b70882bcb3f0be191a92cee38ead3ca6ca26760c
m020202 <m020202@naver.com>
2025-01-20 18:20:33 +0900

    Feat: DTO 생성

---
+++ src/main/java/TImeCAlling/spring/web/dto/checklist/ChecklistRequestDTO.java
@@ -0,0 +1,10 @@
+package TImeCAlling.spring.web.dto.checklist;
+
+import lombok.Getter;
+
+public class ChecklistRequestDTO {
+    @Getter
+    public static class UpdateDTO {
+
+    }
+}
---
+++ src/main/java/TImeCAlling/spring/web/dto/checklist/ChecklistResponseDTO.java
@@ -0,0 +1,7 @@
+package TImeCAlling.spring.web.dto.checklist;
+
+public class ChecklistResponseDTO {
+    public static class UpdateResultDTO {
+
+    }
+}
