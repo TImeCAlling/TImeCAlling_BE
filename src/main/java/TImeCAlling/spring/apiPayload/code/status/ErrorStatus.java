9b88c7bb42b77d7f4f6d7ae6bc863aefe61c3904
KYS <ehcl1027@gmail.com>
2025-01-20 17:35:49 +0900

    chore: 일정의 공유 아이디 관련 ErrorStatus 추가

--- src/main/java/TImeCAlling/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/TImeCAlling/spring/apiPayload/code/status/ErrorStatus.java
@@ -37,7 +37,8 @@ public enum ErrorStatus implements BaseErrorCode {
     // 일정 관련 에러
     SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "SCHEDULE4001", "일정을 찾을 수 없습니다."),
     SCHEDULE_DELETE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "SCHEDULE4002", "일정 삭제에 실패했습니다."),
-    RECURRING_SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "SCHEDULE4003", "반복 일정 정보가 정상적으로 저장되지 않았습니다.")
+    RECURRING_SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "SCHEDULE4003", "반복 일정 정보가 정상적으로 저장되지 않았습니다."),
+    SCHEDULE_SHARE_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "SCHEDULE4004", "일정의 공유 ID를 찾을 수 업습니다.")
     ;
 
     private final HttpStatus httpStatus;
