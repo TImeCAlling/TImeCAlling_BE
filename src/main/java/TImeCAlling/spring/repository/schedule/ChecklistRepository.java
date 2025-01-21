eaeffe2c47baad7d3fc75fe40626de7a7c0af379
m020202 <m020202@naver.com>
2025-01-21 10:11:50 +0900

    Fix: executeQuery -> executeUpdate로 변경

--- src/main/java/TImeCAlling/spring/repository/schedule/ChecklistRepository.java
+++ src/main/java/TImeCAlling/spring/repository/schedule/ChecklistRepository.java
@@ -2,6 +2,7 @@ package TImeCAlling.spring.repository.schedule;
 
 import TImeCAlling.spring.domain.Checklist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,8 +15,9 @@ public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
 
     Checklist findByScheduleIdAndDate(Long scheduleId, LocalDate date);
 
+    @Modifying
     @Query(
-            value = "DELETE from Checklist where date < :date and isWritten = true",
+            value = "DELETE from Checklist where date < :date and is_written = true",
             nativeQuery = true
     )
     void deleteExpiredChecklists(@Param("date") LocalDate date);
