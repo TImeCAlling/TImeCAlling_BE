ffb65b65d97ea414cbd34f0b45b0db4f052188a0
yunsful <bailey05280@naver.com>
2025-01-21 17:02:04 +0900

    feat: 리파지토리 작성

--- src/main/java/TImeCAlling/spring/repository/schedule/ChecklistRepository.java
+++ src/main/java/TImeCAlling/spring/repository/schedule/ChecklistRepository.java
@@ -2,9 +2,20 @@ package TImeCAlling.spring.repository.schedule;
 
 import TImeCAlling.spring.domain.Checklist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
     
     void deleteAllByScheduleId(Long scheduleId);
-
+    
+    @Query("SELECT c FROM Checklist c " +
+            "JOIN c.schedule s " +
+            "WHERE s.user.id = :userId " +
+            "AND c.date = :date")
+    List<Checklist> findChecklistsByUserAndDate(@Param("userId") Long userId,
+                                              @Param("date") LocalDate date);
 }
