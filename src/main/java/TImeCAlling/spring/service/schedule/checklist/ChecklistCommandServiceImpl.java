4283e2ad5e3d45b3baf8ae79ad7947227e710306
yunsful <bailey05280@naver.com>
2025-01-22 17:52:20 +0900

    chore: 체크리스트 서비스 메서드 이동

--- src/main/java/TImeCAlling/spring/service/schedule/checklist/ChecklistCommandServiceImpl.java
+++ src/main/java/TImeCAlling/spring/service/schedule/checklist/ChecklistCommandServiceImpl.java
@@ -129,12 +129,7 @@ public class ChecklistCommandServiceImpl implements ChecklistCommandService {
 
         return checklists;
     }
-
     
-    @Override
-    public List<Checklist> getCheckListByDateAndUser(LocalDate date, User user) {
-        return checklistRepository.findChecklistsBySchedule_User_IdAndDate(user.getId(), date);
-    }
     
     private List<LocalDate> calculateRepeatDates(LocalDate start, LocalDate end, java.util.List<RepeatDay> repeatDays) {
         List<LocalDate> repeatDates = new ArrayList<>();
@@ -160,6 +155,8 @@ public class ChecklistCommandServiceImpl implements ChecklistCommandService {
 
         return checklist.getId();
     }
+    
+    
 
     @Override
     @Scheduled(cron = "0 0 0 * * *")
--- src/main/java/TImeCAlling/spring/service/schedule/checklist/ChecklistQueryService.java
+++ src/main/java/TImeCAlling/spring/service/schedule/checklist/ChecklistQueryService.java
@@ -1,7 +1,14 @@
 package TImeCAlling.spring.service.schedule.checklist;
 
 import TImeCAlling.spring.domain.Checklist;
+import TImeCAlling.spring.domain.User;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ChecklistQueryService {
     Checklist findOne(Long checklistId);
+    
+    List<Checklist> getCheckListByDateAndUser(LocalDate date, User user);
+    
 }
