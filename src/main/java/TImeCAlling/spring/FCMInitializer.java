e8876a588b5d13d03c764c1572fdf26e686613a5
KYS <ehcl1027@gmail.com>
2025-01-22 09:03:15 +0900

    feat: 서버 시작시 FCM을 등록하는 컴포넌트 추가

---
+++ src/main/java/TImeCAlling/spring/FCMInitializer.java
@@ -0,0 +1,38 @@
+package TImeCAlling.spring;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class FCMInitializer {
+
+    @Value("${fcm.firebase_config_path}")
+    private String firebaseConfigPath;
+
+    @PostConstruct // 빈 객체가 생성되고 의존성 주입이 완료된 후에 실행될 수 있도록 설정
+    public void initialize() {
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(googleCredentials)
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("Firebase application has been initialized");
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
