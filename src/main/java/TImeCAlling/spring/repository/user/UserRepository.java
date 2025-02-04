cc12c76bbcecde7454a9a3399f916f4905bbf6bc
KYS <ehcl1027@gmail.com>
2025-02-04 14:34:43 +0900

    feat: 유저id를 기반으로 Fcm토큰 반환 메소드

--- src/main/java/TImeCAlling/spring/repository/user/UserRepository.java
+++ src/main/java/TImeCAlling/spring/repository/user/UserRepository.java
@@ -2,6 +2,7 @@ package TImeCAlling.spring.repository.user;
 
 import TImeCAlling.spring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +10,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNickname(String nickname);
     Optional<User> findBySocialId(Long socialId);
     Optional<User> findByRefreshToken(String refreshToken);
+
+    @Query("SELECT u.fcmToken FROM User u WHERE u.id = :userId")
+    Optional<String> findFcmTokenByUserId(Long userId);
 }
