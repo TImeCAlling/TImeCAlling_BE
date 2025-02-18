3fe216bf8f0d6a5d44ef4be12369dc6cf31adcb8
KYS <ehcl1027@gmail.com>
2025-02-18 12:07:21 +0900

    fix: 어노테이션 추가로 400request 해결

--- src/main/java/TImeCAlling/spring/web/dto/fcm/FcmTokenRequestDTO.java
+++ src/main/java/TImeCAlling/spring/web/dto/fcm/FcmTokenRequestDTO.java
@@ -1,11 +1,15 @@
 package TImeCAlling.spring.web.dto.fcm;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class FcmTokenRequestDTO {
 
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Getter
     public static class UpdateDTO {
         private String fcmToken;
