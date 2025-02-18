d9496a98d09268f9312dd4a6014033b2a0c272c5
KYS <ehcl1027@gmail.com>
2025-02-18 15:10:14 +0900

    chore: 깨우기 requestDTO 빌더 패턴 추가

--- src/main/java/TImeCAlling/spring/web/dto/pushMessageNotification/PushNotificationRequestDTO.java
+++ src/main/java/TImeCAlling/spring/web/dto/pushMessageNotification/PushNotificationRequestDTO.java
@@ -1,10 +1,16 @@
 package TImeCAlling.spring.web.dto.pushMessageNotification;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class PushNotificationRequestDTO {
 
     @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class NotificationDetails {
         private Long receiverId;
         private String shareId;
